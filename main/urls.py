fd92350443cfc5623773136eabdf740da9a0e10f
Vasilii Ledkov <alk-rk@yandex.ru>
2023-07-16 14:11:30 +0300

    add products to home page

--- main/urls.py
+++ main/urls.py
@@ -1,2 +1,10 @@
+from django.urls import path
 
+from main.views import index, contacts
+
+
+urlpatterns = [
+    path('', index),
+    path('contacts/', contacts),
+]
 
--- main/views.py
+++ main/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+from main.models import Product
+
+
+def index(request):
+    product_list = Product.objects.all()
+    context = {
+        'object_list': product_list,
+    }
+    return render(request, 'main/index.html', context)
+
+
+def contacts(request):
+
+    if request == "POST":
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(f"{name} ({phone}) - {message}")
+
+    return render(request, 'main/contacts.html')
