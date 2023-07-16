00d08ab172d323e105fdfd3e7339833e49758aba
Vasilii Ledkov <alk-rk@yandex.ru>
2023-07-16 10:52:27 +0300

    second commit fixed errors

--- main/management/commands/fill_category.py
+++ main/management/commands/fill_category.py
@@ -1,4 +1,7 @@
+import json
+
 from django.core.management import BaseCommand
+from main.models import Category
 
 
 class Command(BaseCommand):
@@ -6,8 +9,10 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         category_for_create = []
         with open('data.json', 'r', encoding='UTF8') as data:
+            data = json.loads(data.read())
             for cat_item in data:
                 category_for_create.append(
-                    Category(**cat_item)
+                    Category(**cat_item['fields'])
                 )
-            Category.objects.bulk_create(category_for_create)
+
+        Category.objects.bulk_create(category_for_create)
--- main/models.py
+++ main/models.py
@@ -4,23 +4,6 @@ from django.db import models
 NULLABLE = {'blank': True, 'null': True}
 
 
-class Product(models.Model):
-    prod_title = models.CharField(max_length=100, verbose_name='наименование товара')
-    prod_description = models.TextField(verbose_name='описание', **NULLABLE)
-    preview_image = models.ImageField(upload_to='prev_image/', verbose_name='изображение (превью)', **NULLABLE)
-    category = models.CharField(max_length=100, verbose_name='категория')
-    price = models.IntegerField(verbose_name='цена за покупку')
-    creation_date = models.DateField(verbose_name='дата создания', auto_now=True)
-    last_update = models.DateField(verbose_name='дата последнего изменения', auto_now_add=True)
-
-    def __str__(self):
-        return f'{self.prod_title} - {self.price}'
-
-    class Meta:
-        verbose_name = 'продукт'
-        verbose_name_plural = 'продукты'
-
-
 class Category(models.Model):
     category = models.CharField(max_length=100, verbose_name='категория')
     description = models.TextField(verbose_name='описание', **NULLABLE)
@@ -31,3 +14,20 @@ class Category(models.Model):
     class Meta:
         verbose_name = 'категория'
         verbose_name_plural = 'категории'
+
+
+class Product(models.Model):
+    prod_title = models.CharField(max_length=100, verbose_name='наименование товара')
+    prod_description = models.TextField(verbose_name='описание', **NULLABLE)
+    preview_image = models.ImageField(upload_to='prev_image/', verbose_name='изображение (превью)', **NULLABLE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='категория')
+    price = models.IntegerField(verbose_name='цена за покупку')
+    creation_date = models.DateField(verbose_name='дата создания', auto_now=True)
+    last_update = models.DateField(verbose_name='дата последнего изменения', auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.prod_title} - {self.price}'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
